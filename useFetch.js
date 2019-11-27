d57c458f49275bfd293237b6244467a2d84d409e
Eddy H <48016781+EddyHamzich@users.noreply.github.com>
2019-11-27 07:33:16 +0200

    Create useFetch.js

---
+++ useFetch.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+export const useFetch = (url) => {
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    
+    useEffect(() => {
+        (async () => {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            setData(data);
+            setLoading(false);
+        }
+        catch (error) {
+            setError(error)
+            setLoading(true);
+        }
+      })()
+      //eslint-disable-next-line
+    }, []);
+  
+    return { loading, data, error };
+};
+
+/* Usage in component:
+
+export default function App() {
+    
+    const { loading, data, error } = useFetch("https://jsonplaceholder.typicode.com/todos/1");
+    
+    return (
+        <div>
+            { loading ? (<div>...loading{error && console.log(error)}</div>) : (<div>{data.title}</div>) }
+        </div>
+    );
+};
+
+// "error &&" checks if error actually exists (truthy/falsy trick)
+
+*/
