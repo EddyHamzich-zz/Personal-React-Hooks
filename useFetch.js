68dc0645f73742b058f564bf3c03b8fe43c6ae81
Eddy H <48016781+EddyHamzich@users.noreply.github.com>
2019-11-27 07:42:32 +0200

    ...

--- useFetch.js
+++ useFetch.js
@@ -7,17 +7,17 @@ export const useFetch = (url) => {
     
     useEffect(() => {
         (async () => {
-        try {
-            const response = await fetch(url);
-            const data = await response.json();
-            setData(data);
-            setLoading(false);
-        }
-        catch (error) {
-            setError(error)
-            setLoading(true);
-        }
-      })()
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                setData(data);
+                setLoading(false);
+            }
+            catch (error) {
+                setError(error)
+                setLoading(true);
+            }
+        })()
       //eslint-disable-next-line
     }, []);
   
